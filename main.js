b1d356a6a582b70bac685644cda47c9b3462d1ce
Rodrigo Martins <84294260+Pulista@users.noreply.github.com>
2023-09-28 10:59:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 const indicator = document.querySelector(".indicator");
-addEventListener("scroll", () => {
-  const { scrollHeight } = document.body;
-  const { innerHeight, innerWidth } = window;
-  const left = (scrollY / scrollHeight) * innerWidth;
-  indicator.computedStyleMap.transform = `translateX(${left}px)`;
+const btnTop = document.querySelector("#btnTop");
+
+// Criar um observador de interseção para monitorar os elementos na janela
+const observer = new IntersectionObserver((entries) => {
+  // Para cada entrada, verificar se está intersectando ou não
+  for (const entry of entries) {
+    // Se o elemento é o indicator, atualizar sua posição
+    if (entry.target === indicator) {
+      const { scrollHeight } = document.body;
+      const { innerWidth } = window;
+      const left = (window.pageYOffset / scrollHeight) * innerWidth;
+      indicator.style.transform = `translateX(${left}px)`;
+    }
+    // Se o elemento é o btnTop, atualizar seu estilo de exibição
+    if (entry.target === btnTop) {
+      if (entry.isIntersecting) {
+        btnTop.style.display = "none";
+      } else {
+        btnTop.style.display = "block";
+      }
+    }
+  }
 });
+
+// Observar os elementos na janela
+observer.observe(indicator);
+observer.observe(btnTop);
+
+function scrollToElement(elementId) {
+  const element = document.querySelector(`#${elementId}`);
+  if (element) {
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+}
